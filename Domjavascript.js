198a2edfbfecf2c55f5415ecce00c5412f78ad98
Arshadsidqi <arshad171999@gmail.com>
2024-04-04 13:01:04 +0530

    add js dom selector code

---
+++ Domjavascript.js
@@ -0,0 +1,58 @@
+let body = document.body;
+let items = document.getElementsByClassName("item");
+// console.log(items);
+
+
+// using for in loop
+
+// for (let a in items) {
+//   items[a].style = ` border:2px solid black;
+//                      font-size:25px;
+//                      width:150px;
+//                      border-radius:15px;
+//                      margin:20px;
+//                      background:yellowGreen;
+//                      text-align:center `;
+// }
+
+
+//   using for of loop
+
+// for (ref of items) {
+//   ref.style = ` border:2px solid black;
+//                      font-size:25px;
+//                      width:150px;
+//                      border-radius:15px;
+//                      margin:20px;
+//                      background:yellowGreen;
+//                      text-align:center `;
+// }
+
+// target element by tag name
+
+// let divs=document.getElementsByTagName('div')
+
+// divs[1].style=`border:2px solid black;`;
+// console.log(divs)
+
+//     target element by queary selector
+
+// let divs=document.querySelector('.item')
+// divs.style=`border:2px solid red;width:fit-content;`;
+
+//     target element by queary selector All
+
+ // let divs = document.querySelectorAll(".item");
+// divs[0].style = `border:2px solid red;width:fit-content;`;
+// console.log(divs)
+
+// for (ref of divs) {
+//   ref.style = ` border:2px solid black;
+//                      font-size:25px;
+//                      width:150px;
+//                      border-radius:15px;
+//                      margin:20px;
+//                      background:yellowGreen;
+//                      text-align:center `;
+// }
+
